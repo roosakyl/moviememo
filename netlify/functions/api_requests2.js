4f617a9985f55ef9ee5b14fda43ff96ba49b7043
Roosa Kyllönen <roosa.kyllonen@gmail.com>
2023-11-26 14:09:41 +0200

    Netlify functions tests

--- netlify/functions/api_requests2.js
+++ netlify/functions/api_requests2.js
@@ -1,6 +1,6 @@
 exports.handler = async (event, context) => {
     const data = { hello: 'Hello World!' };
-  
+    console.log(data);
     return {
       statusCode: 200,
       body: JSON.stringify(data),
